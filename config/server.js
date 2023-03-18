b9e7910921fd7b42ac769084974da873f3fb8b96
deepbb <pradeepbb004@gmail.com>
2023-03-18 22:30:50 +0530

    key changes

--- config/server.js
+++ config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('h1kkOc49rN/IBxStP0L57g==,VaxT1I+zZrLC+eq0E7Q4Yw==,L4HEQfhKVoDhTGH/SggXXw==,oFunSnkjNhWGRBx248OrFw=='),
   },
 });
